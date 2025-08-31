ad4c4ad4de1c8f03e74e0e7c144dd455dc9d4bfd
DeyviRodrigo <137321606+DeyviRodrigo@users.noreply.github.com>
2025-08-31 18:38:22 -0500

    Fix shared options relative paths

--- lib/features/calculadora/options/index.dart
+++ lib/features/calculadora/options/index.dart
@@ -1,4 +1,4 @@
-export '../../presentation/shared/menu_option.dart';
-export '../../presentation/shared/general_action.dart';
-export '../../presentation/shared/descuento_action.dart';
-export '../../presentation/shared/ley_action.dart';
+export '../../../presentation/shared/menu_option.dart';
+export '../../../presentation/shared/general_action.dart';
+export '../../../presentation/shared/descuento_action.dart';
+export '../../../presentation/shared/ley_action.dart';
