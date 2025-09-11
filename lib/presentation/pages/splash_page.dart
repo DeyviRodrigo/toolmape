e2163421de11a2d917fa156a4b238a9b5588aa8d
DeyviRodrigo <137321606+DeyviRodrigo@users.noreply.github.com>
2025-09-11 01:11:51 -0500

    Set dark mode default and improve splash

--- lib/presentation/pages/splash_page.dart
+++ lib/presentation/pages/splash_page.dart
@@ -8,28 +8,46 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacementNamed(context, routeCalculadora);
-    });
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )
+      ..addListener(() => setState(() {}))
+      ..forward().whenComplete(
+          () => Navigator.pushReplacementNamed(context, routeCalculadora));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final progress = (_controller.value * 100).round();
     return Scaffold(
+      backgroundColor: isDark ? Colors.black : Colors.grey[200],
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: const [
-            _SplashImage(),
-            SizedBox(height: 24),
+          children: [
+            const _SplashImage(),
+            const SizedBox(height: 24),
             SizedBox(
               width: 200,
-              child: LinearProgressIndicator(),
+              child: LinearProgressIndicator(value: _controller.value),
             ),
+            const SizedBox(height: 8),
+            Text('$progress%'),
           ],
         ),
       ),
--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'themes/index.dart';
 
-final themeModeProvider = StateProvider<ThemeMode>((_) => ThemeMode.system);
-final themeProfileProvider = StateProvider<String>((_) => 'light');
+final themeModeProvider = StateProvider<ThemeMode>((_) => ThemeMode.dark);
+final themeProfileProvider = StateProvider<String>((_) => 'dark');
 
 final themeDataProvider = Provider<ThemeData>((ref) {
   switch (ref.watch(themeProfileProvider)) {
